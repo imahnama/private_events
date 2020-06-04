04c94e1ba6d5eb8e675a2e7d01feb09613b6427b
evansinho <igiri.evanson@gmail.com>
2020-06-04 16:29:49 +0100

    created model for event_attendees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_144733) do
+ActiveRecord::Schema.define(version: 2020_06_04_152449) do
+
+  create_table "event_attendees", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"

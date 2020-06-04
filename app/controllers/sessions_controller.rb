a4f09f9e0914e9b11647648ba45cca126f3fc4e2
imahnama <muannama25@gmail.com>
2020-06-04 12:52:56 +0300

    Add user model, sessions/user controllers and corresponding views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user
+      login_url user
+      redirect_to user_path(user)
+    else
+      flash.now[:notice] = 'Email does not exist'
+      render 'new'
+    end
+  end
+
+    def destroy
+      log_out
+      if logged_in?
+      redirect_to login_path
+      end
+   end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root 'sessions#new'
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
+
+resources :users, only: %i[create new show]
+end
